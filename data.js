f8747ad90f7bf997266831e053a0c190dbcdec8b
domssilva <silva@programmer.net>
2020-08-13 17:33:54 +0200

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,27 @@
+export default videos = [
+  {
+    id: 0,
+    category: "educational",
+    url: "",
+  },
+  {
+    id: 1,
+    category: "career",
+    url: "https://youtu.be/-jF4K4w14Ak",
+  },
+  {
+    id: 2,
+    category: "computer graphics",
+    url: "",
+  },
+  {
+    id: 3,
+    category: "development",
+    url: "",
+  },
+  {
+    id: 4,
+    category: "tech",
+    url: "",
+  },
+]
