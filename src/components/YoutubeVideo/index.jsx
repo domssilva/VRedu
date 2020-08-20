faf6b806dfef87636ffea21f7f95a73e0d9bcd06
domssilva <silva@programmer.net>
2020-08-20 20:57:48 +0200

    improve iframe loading performance

--- src/components/YoutubeVideo/index.jsx
+++ src/components/YoutubeVideo/index.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
-import { AiOutlineFullscreen ,AiFillYoutube } from "react-icons/ai";
+import { AiOutlineFullscreen ,AiFillYoutube } from 'react-icons/ai';
 
 import YoutubeVideoModal from '../YoutubeVideoModal';
 import './styles.scss';
 
-const YoutubeVideo = (props) => {
+const YoutubeVideo = ({ videoid, name }) => {
 
   const [modalIsOpen, setIsOpen] = useState(false);
 
@@ -16,26 +16,30 @@ const YoutubeVideo = (props) => {
     setIsOpen(false);
   }
 
-  const iframeLink = `https://www.youtube.com/embed/${props.videoid}`;
+  const iframeLink = `https://www.youtube.com/embed/${videoid}`;
 
   return (
     <>
       <YoutubeVideoModal 
         videoLink={iframeLink}
+        videoId={videoid}
         modalIsOpen={modalIsOpen} 
         closeModal={closeModal}
       />
-      <div className="youtube__video">
-        <div className="youtube__tab">
-          <a href={`https://www.youtube.com/watch?v=${props.videoid}`} target='_blank' rel="noopener noreferrer">
-            <AiFillYoutube className="youtube__icon"/>
+      <div className='youtube__video'>
+        <div className='youtube__tab'>
+          <a href={`https://www.youtube.com/watch?v=${videoid}`} target='_blank' rel='noopener noreferrer'>
+            <AiFillYoutube className='youtube__icon'/>
           </a>
-          <AiOutlineFullscreen onClick={openModal} className="youtube__icon"/>
+          <AiOutlineFullscreen onClick={openModal} className='youtube__icon'/>
         </div>
         <iframe 
-          title={props.name}
-          src={iframeLink}
+          title={name}
           frameBorder='0'
+          width='100%'
+          height='100%'
+          allow='autoplay'
+          srcdoc={`<html style='overflow: hidden;'><a href=https://www.youtube.com/embed/${videoid}?autoplay=1><img style='width: 100%; height: 100%; object-fit: cover;' src=https://img.youtube.com/vi/${videoid}/hqdefault.jpg></a></html>`}
           allowFullScreen
         />
       </div>
--- src/components/YoutubeVideoModal/index.jsx
+++ src/components/YoutubeVideoModal/index.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import Modal from 'react-modal';
-import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineClose } from 'react-icons/ai';
 
 import './styles.scss';
 
 Modal.setAppElement('#root');
 
-const YoutubeVideoModal = ({ videoLink, modalIsOpen, closeModal, afterOpenModal }) => (
+const YoutubeVideoModal = ({ videoId, videoLink, modalIsOpen, closeModal, afterOpenModal }) => (
     <Modal
       isOpen={modalIsOpen}
       onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
-      contentLabel="Fullscreen Video"
-      className="Modal"
+      contentLabel='Fullscreen Video'
+      className='Modal'
       >
-      <div className="Modal__tab">
-        <button className="Modal__close" onClick={closeModal}>
-          <AiOutlineClose className="youtube__icon"/>
+      <div className='Modal__tab'>
+        <button className='Modal__close' onClick={closeModal}>
+          <AiOutlineClose className='youtube__icon'/>
         </button>
       </div>
-      <div className="Modal__body">
-        <iframe title="Fullscreen video" src={videoLink}/>
+      <div className='Modal__body'>
+        <iframe 
+          title='Fullscreen video' 
+          width='100%'
+          height='100%'
+          allow='autoplay'
+          srcdoc={`<html style='object-fit: cover; overflow: hidden;'><a href=https://www.youtube.com/embed/${videoId}?autoplay=1><img style='width: 100%; height: 100%; object-fit: cover;' src=https://img.youtube.com/vi/${videoId}/hqdefault.jpg></a></html>`}
+          allowFullScreen
+        />
       </div>
     </Modal>
 );
