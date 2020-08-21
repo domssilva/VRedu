7d9105ef10cda35c743ae07c887d81a14b027d00
domssilva <silva@programmer.net>
2020-08-21 13:49:11 +0200

    implement dropdown menu for mobile

---
+++ src/components/Dropdown/index.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import { FaAlignJustify } from 'react-icons/fa'
+
+import './styles.scss';
+
+const Backdrop = ({closeDropdown}) => (
+    <div onClick={closeDropdown} className="backdrop"></div>
+  )
+
+const Dropdown = () => {
+
+  const [open, setOpen] = useState(false);
+
+  function handleDropdown() {
+    setOpen(!open);
+  }
+
+  function closeDropdown() {
+    setOpen(false);
+  }
+
+  return (
+    <section className='dropdown'>
+        <Backdrop closeDropdown={closeDropdown}/>
+        <FaAlignJustify onClick={handleDropdown} className='dropdown__icon'/>
+        <div className={`dropdown__container ${(open ? '' : 'closed')}`}>
+          <Link className='dropdown__item' to='/' title='vr news'>Home</Link>
+          <Link className='dropdown__item' to='/news' title='vr news'>News</Link>
+          <Link className='dropdown__item' to='/videos' title='watch vr content'>Videos</Link>
+          <Link className='dropdown__item' to='/resources' title='start learning!'>Resources</Link>
+        </div>
+    </section>
+  )
+}
+
+export default Dropdown;
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Dropdown from '../Dropdown';
+
 import './styles.scss';
 
 export default function Navigation() {
   return (
     <nav role='navigation' tabIndex='0' className='navigation'>
       <div className='navigation__left'>
-        <Link to='/' className='logo'>VR-edu</Link>
+        <Link to='/' className='navigation__logo'>VR-edu</Link>
       </div>
-      <div className='navigation__right'>
+      <div className='navigation__right desktop'>
         <Link to='/news' title='vr news'>News</Link>
         <Link to='/videos' title='watch vr content'>Videos</Link>
         <Link to='/resources' title='start learning!'>Resources</Link>
+      </div>
+      <div className='navigation__right mobile'>
+        <Dropdown/>
       </div>
     </nav>
   )
